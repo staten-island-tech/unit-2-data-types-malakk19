e3507fcce4fadeebd7f425318a5d792244e020a3
malakk19 <158211975+malakk19@users.noreply.github.com>
2024-02-07 13:57:44 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,18 +1,15 @@
 #integers, booleans, strings
 #challange 1
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
-print(5420%2)
-print(363%2)
+
 #challange 2 
-tip = 100
-if tip == 0:
-    print('bad')
-elif tip == 15:
-    print('okay')
-elif tip == 20:
-    print('good')
-elif tip == 25:
-    print('great')
+x = (input("what is the bill"))
+bill = input("How was the service?")
+if service == "bad":
+    print(x)
+    if service == "okay":
+    print(x)
+if service == "good":
+    print(x)
+if service == "great":
+    print(x)
+
