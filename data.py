cc8eed539b58dac7e9faef6f00dc73f5ff1e77e4
malakk19 <158211975+malakk19@users.noreply.github.com>
2024-02-08 14:00:31 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,15 +1,31 @@
 #integers, booleans, strings
 #challange 1
+""" x = (int(input("Is it even or odd?")))
+if (x%2) > 0:
+    print("odd")
+else:
+    print("even")
 
-#challange 2 
-x = (input("what is the bill"))
-bill = input("How was the service?")
+##challange 2
+
+x = int(input("What is the bill?"))
+service = input("how was the service? ")
 if service == "bad":
     print(x)
-    if service == "okay":
-    print(x)
+if service == "okay":
+    print(x + (x%15))
 if service == "good":
-    print(x)
+    print(x + (x%20))
 if service == "great":
-    print(x)
+    print(x+ (x%25))
 
+    ##challenge 3 """
+def allfactors (n):
+    factors = []
+    for i in range(1,n+1):
+        if n%i == 0:
+            factors.append(i)
+            return factors
+number = int(input("please enter a number: "))
+listfactors = allfactors(number)
+print(listfactors)
